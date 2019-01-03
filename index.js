ab2ae6f969de146eb01b6aaf106bebbe828da163
Hapachino <erickwbrownfield@gmail.com>
2019-01-02 18:23:39 -0800

    basic server setup complete

--- index.js
+++ index.js
@@ -1,44 +1,16 @@
 const express = require('express');
 const cors = require('cors');
 const PORT = process.env.PORT || 9000;
+const { resolve } = require('path');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(express.static(resolve(__dirname, 'client', 'dist')));
 
-app.get('/', (req, res) => {
-  res.send('<h1>Working</h1>');
-});
-
-app.get('/api/test', (req, res) => {
-  const data = {
-    success: true,
-    message: 'Test API working',
-    another: 'just a test',
-  };
-
-  res.send(data);
-});
-
-app.get('/api/user', (req, res) => {
-  const user = {
-    email: 'john@mail.com',
-    name: 'John Doe',
-    username: 'john',
-  };
-
-  res.send(user);
-});
-
-app.post('/api/send-message', (req, res) => {
-  console.log('Data from client:', req.body);
-
-  res.send({
-    success: true,
-    dataReceived: req.body,
-  });
-});
+require('./routes')(app);
 
 app.listen(PORT, () => {
   console.log('Server running on PORT', PORT);
